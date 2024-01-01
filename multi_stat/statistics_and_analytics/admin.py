3d5905b22dc0cda6236a6fc132deb3e3b14fdc50
Roman <121165911+madmanRE@users.noreply.github.com>
2024-01-01 23:27:19 +0300

    make download data to db

--- multi_stat/statistics_and_analytics/admin.py
+++ multi_stat/statistics_and_analytics/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import SEOReport, SEOReportDetail
+from .models import (
+    SEOReport,
+    YandexSEOReport,
+    GoogleSEOReport,
+    DirectSEOReport,
+    ReferralsSEOReport,
+    YandexDirect,
+    GoogleAds,
+    VKAds,
+    FacebookAds,
+    InstagramAds,
+    MyTarget,
+    TelegramAds,
+    EmailTracking,
+    CallTracking,
+)
 
 
 @admin.register(SEOReport)
@@ -15,8 +30,8 @@ class SEOReportAdmin(admin.ModelAdmin):
     ]
 
 
-@admin.register(SEOReportDetail)
-class SEOReportDetailAdmin(admin.ModelAdmin):
+@admin.register(GoogleSEOReport)
+class GoogleSEOReportAdmin(admin.ModelAdmin):
     list_display = [
         "title",
         "visits",
@@ -28,3 +43,192 @@ class SEOReportDetailAdmin(admin.ModelAdmin):
         "created",
         "category",
     ]
+
+
+@admin.register(YandexSEOReport)
+class YandexSEOReportAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "visits",
+        "impressions",
+        "ctr",
+        "avg_position",
+        "convert",
+        "transactions",
+        "created",
+        "category",
+    ]
+
+
+@admin.register(DirectSEOReport)
+class DirectSEOReportAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "visits",
+        "impressions",
+        "ctr",
+        "avg_position",
+        "convert",
+        "transactions",
+        "created",
+        "category",
+    ]
+
+
+@admin.register(ReferralsSEOReport)
+class ReferralsSEOReportAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "visits",
+        "impressions",
+        "ctr",
+        "avg_position",
+        "convert",
+        "transactions",
+        "created",
+        "category",
+    ]
+
+
+@admin.register(YandexDirect)
+class YandexDirectAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "views",
+        "visits",
+        "average_check",
+        "ctr",
+        "cta",
+        "transactions",
+        "all_expense",
+        "income",
+        "created",
+    ]
+    list_filter = ["all_expense", "income"]
+
+
+@admin.register(GoogleAds)
+class GoogleAdsAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "views",
+        "visits",
+        "average_check",
+        "ctr",
+        "cta",
+        "transactions",
+        "all_expense",
+        "income",
+        "created",
+    ]
+    list_filter = ["all_expense", "income"]
+
+
+@admin.register(VKAds)
+class VKAdsAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "views",
+        "visits",
+        "average_check",
+        "ctr",
+        "cta",
+        "transactions",
+        "all_expense",
+        "income",
+        "created",
+    ]
+    list_filter = ["all_expense", "income"]
+
+
+@admin.register(FacebookAds)
+class FacebookAdsAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "views",
+        "visits",
+        "average_check",
+        "ctr",
+        "cta",
+        "transactions",
+        "all_expense",
+        "income",
+        "created",
+    ]
+    list_filter = ["all_expense", "income"]
+
+
+@admin.register(InstagramAds)
+class InstagramAdsAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "views",
+        "visits",
+        "average_check",
+        "ctr",
+        "cta",
+        "transactions",
+        "all_expense",
+        "income",
+        "created",
+    ]
+    list_filter = ["all_expense", "income"]
+
+
+@admin.register(MyTarget)
+class MyTargetAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "views",
+        "visits",
+        "average_check",
+        "ctr",
+        "cta",
+        "transactions",
+        "all_expense",
+        "income",
+        "created",
+    ]
+    list_filter = ["all_expense", "income"]
+
+
+@admin.register(TelegramAds)
+class TelegramAdsAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "visits",
+        "convert",
+        "transactions",
+        "all_expense",
+        "income",
+        "created",
+    ]
+    list_filter = ["all_expense", "income"]
+
+
+@admin.register(EmailTracking)
+class EmailTrackingAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "visits",
+        "convert",
+        "transactions",
+        "all_expense",
+        "income",
+        "created",
+    ]
+    list_filter = ["all_expense", "income"]
+
+
+@admin.register(CallTracking)
+class CallTrackingAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "visits",
+        "convert",
+        "transactions",
+        "all_expense",
+        "income",
+        "created",
+    ]
+    list_filter = ["all_expense", "income"]
--- multi_stat/statistics_and_analytics/dataloader.py
+++ multi_stat/statistics_and_analytics/dataloader.py
@@ -1,20 +1,242 @@
 import requests
-from .models import SEOReport, SEOReportDetail
+from .models import (
+    SEOReport,
+    YandexSEOReport,
+    GoogleSEOReport,
+    DirectSEOReport,
+    ReferralsSEOReport,
+    YandexDirect,
+    GoogleAds,
+    VKAds,
+    FacebookAds,
+    InstagramAds,
+    MyTarget,
+    TelegramAds,
+    EmailTracking,
+    CallTracking,
+)
 
 
 def get_seo_reports():
     url = "http://127.0.0.1:8000/seo/"
     response = requests.get(url)
     res_json = response.json()
+    seo_report = SEOReport.objects.create(
+        title=res_json["title"],
+        visits=res_json["visits"],
+        all_expense=res_json["all_expense"],
+        sum_transactions=res_json["sum_transactions"],
+        avg_check=res_json["avg_check"],
+        income=res_json["income"],
+    )
+    seo_report.save()
+    yandex_report = YandexSEOReport.objects.create(
+        title=res_json["yandex"]["title"],
+        visits=res_json["yandex"]["visits"],
+        impressions=res_json["yandex"]["impressions"],
+        ctr=res_json["yandex"]["ctr"],
+        avg_position=res_json["yandex"]["avg_position"],
+        convert=res_json["yandex"]["convert"],
+        transactions=res_json["yandex"]["transactions"],
+        category=SEOReport.objects.filter(title="SEO")[0],
+    )
+    yandex_report.save()
+    google_report = GoogleSEOReport.objects.create(
+        title=res_json["google"]["title"],
+        visits=res_json["google"]["visits"],
+        impressions=res_json["google"]["impressions"],
+        ctr=res_json["google"]["ctr"],
+        avg_position=res_json["google"]["avg_position"],
+        convert=res_json["google"]["convert"],
+        transactions=res_json["google"]["transactions"],
+        category=SEOReport.objects.filter(title="SEO")[0],
+    )
+    google_report.save()
+    direct_report = DirectSEOReport.objects.create(
+        title=res_json["direct"]["title"],
+        visits=res_json["direct"]["visits"],
+        convert=res_json["direct"]["convert"],
+        transactions=res_json["direct"]["transactions"],
+        category=SEOReport.objects.filter(title="SEO")[0],
+    )
+    direct_report.save()
+    refs_report = ReferralsSEOReport.objects.create(
+        title=res_json["referrals"]["title"],
+        visits=res_json["referrals"]["visits"],
+        convert=res_json["referrals"]["convert"],
+        transactions=res_json["referrals"]["transactions"],
+        category=SEOReport.objects.filter(title="SEO")[0],
+    )
+    refs_report.save()
     return 0
 
 
+def get_yandex_direct():
+    url = "http://127.0.0.1:8000/yandex_ads/"
+    response = requests.get(url)
+    res_json = response.json()
+    res = YandexDirect.objects.create(
+        title="YandexDirect",
+        views=res_json["views"],
+        visits=res_json["visits"],
+        average_check=res_json["average_check"],
+        ctr=res_json["ctr"],
+        cta=res_json["cta"],
+        transactions=res_json["transactions"],
+        all_expense=res_json["all_expense"],
+        income=res_json["income"],
+    )
+    res.save()
+
+
+def get_google_ads():
+    url = "http://127.0.0.1:8000/google_ads/"
+    response = requests.get(url)
+    res_json = response.json()
+    res = GoogleAds.objects.create(
+        title="GoogleAds",
+        views=res_json["views"],
+        visits=res_json["visits"],
+        average_check=res_json["average_check"],
+        ctr=res_json["ctr"],
+        cta=res_json["cta"],
+        transactions=res_json["transactions"],
+        all_expense=res_json["all_expense"],
+        income=res_json["income"],
+    )
+    res.save()
+
+
+def get_vk_ads():
+    url = "http://127.0.0.1:8000/vk_ads/"
+    response = requests.get(url)
+    res_json = response.json()
+    res = VKAds.objects.create(
+        title="VKAds",
+        views=res_json["views"],
+        visits=res_json["visits"],
+        average_check=res_json["average_check"],
+        ctr=res_json["ctr"],
+        cta=res_json["cta"],
+        transactions=res_json["transactions"],
+        all_expense=res_json["all_expense"],
+        income=res_json["income"],
+    )
+    res.save()
+
+
+def get_mytarget():
+    url = "http://127.0.0.1:8000/vk_ads/"
+    response = requests.get(url)
+    res_json = response.json()
+    res = MyTarget.objects.create(
+        title="MyTarget",
+        views=res_json["views"],
+        visits=res_json["visits"],
+        average_check=res_json["average_check"],
+        ctr=res_json["ctr"],
+        cta=res_json["cta"],
+        transactions=res_json["transactions"],
+        all_expense=res_json["all_expense"],
+        income=res_json["income"],
+    )
+    res.save()
+
+
+def get_facebook_ads():
+    url = "http://127.0.0.1:8000/facebook_ads/"
+    response = requests.get(url)
+    res_json = response.json()
+    res = FacebookAds.objects.create(
+        title="FacebookAds",
+        views=res_json["views"],
+        visits=res_json["visits"],
+        average_check=res_json["average_check"],
+        ctr=res_json["ctr"],
+        cta=res_json["cta"],
+        transactions=res_json["transactions"],
+        all_expense=res_json["all_expense"],
+        income=res_json["income"],
+    )
+    res.save()
+
+
+def get_insta_ads():
+    url = "http://127.0.0.1:8000/facebook_ads/"
+    response = requests.get(url)
+    res_json = response.json()
+    res = InstagramAds.objects.create(
+        title="InstagramAds",
+        views=res_json["views"],
+        visits=res_json["visits"],
+        average_check=res_json["average_check"],
+        ctr=res_json["ctr"],
+        cta=res_json["cta"],
+        transactions=res_json["transactions"],
+        all_expense=res_json["all_expense"],
+        income=res_json["income"],
+    )
+    res.save()
+
+
 def get_ads_reports():
-    pass
+    get_yandex_direct()
+    get_google_ads()
+    get_vk_ads()
+    get_mytarget()
+    get_facebook_ads()
+    get_insta_ads()
+
+
+def get_telegram_ads():
+    url = "http://127.0.0.1:8000/telegram_ads/"
+    response = requests.get(url)
+    res_json = response.json()
+    res = TelegramAds.objects.create(
+        title="TelegramAds",
+        visits=res_json["visits"],
+        convert=res_json["convert"],
+        transactions=res_json["transactions"],
+        all_expense=res_json["all_expense"],
+        income=res_json["income"],
+    )
+    res.save()
+
+
+def get_email_tracking():
+    url = "http://127.0.0.1:8000/email_ads/"
+    response = requests.get(url)
+    res_json = response.json()
+    res = EmailTracking.objects.create(
+        title="EmailTracking",
+        visits=res_json["visits"],
+        convert=res_json["convert"],
+        transactions=res_json["transactions"],
+        all_expense=res_json["all_expense"],
+        income=res_json["income"],
+    )
+    res.save()
+
+
+def get_call_tracking():
+    url = "http://127.0.0.1:8000/call_ads/"
+    response = requests.get(url)
+    res_json = response.json()
+    res = CallTracking.objects.create(
+        title="CallTracking",
+        visits=res_json["visits"],
+        convert=res_json["convert"],
+        transactions=res_json["transactions"],
+        all_expense=res_json["all_expense"],
+        income=res_json["income"],
+    )
+    res.save()
 
 
 def get_other_reports():
-    pass
+    get_telegram_ads()
+    get_email_tracking()
+    get_call_tracking()
 
 
 def dataloader():
