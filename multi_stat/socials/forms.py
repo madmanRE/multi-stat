331364915ab2e3d197f80f0afac734b14b394225
Roman <121165911+madmanRE@users.noreply.github.com>
2024-01-01 14:45:12 +0300

    done registration

--- multi_stat/socials/forms.py
+++ multi_stat/socials/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField()
+
+    class Meta:
+        model = Profile
+        fields = ("username", "phone", "email")
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ("username", "phone", "email", "avatar")
--- multi_stat/socials/models.py
+++ multi_stat/socials/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="профиль")
+    username = models.CharField(max_length=100, default="-------")
+    password = models.CharField(max_length=20, default="1111")
+    phone = models.CharField(
+        max_length=20, blank=True, null=True, verbose_name="телефон"
+    )
+    email = models.EmailField(unique=True, verbose_name="электронная почта")
+    avatar = models.ImageField(
+        upload_to="profiles/profile",
+        null=True,
+        default="profiles/profile/ava-default.png",
+    )
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
