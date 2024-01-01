007e631185303e6fd78f362395bcbcdaa2ef8638
Roman <121165911+madmanRE@users.noreply.github.com>
2024-01-02 01:40:02 +0300

    add smth

--- multi_stat/statistics_and_analytics/views.py
+++ multi_stat/statistics_and_analytics/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from .models import SEOReport, SEOReportDetail
+from .models import (
+    SEOReport,
+    YandexSEOReport,
+    GoogleSEOReport,
+    DirectSEOReport,
+    ReferralsSEOReport,
+    YandexDirect,
+    GoogleAds,
+    VKAds,
+    FacebookAds,
+    InstagramAds,
+    MyTarget,
+    TelegramAds,
+    EmailTracking,
+    CallTracking,
+)
 
 
 @login_required()
