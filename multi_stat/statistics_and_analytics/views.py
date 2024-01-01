bf7cee7be2b3295fcc808535d5e716781e380f50
Roman <121165911+madmanRE@users.noreply.github.com>
2024-01-01 20:00:05 +0300

    start db creation

--- multi_stat/statistics_and_analytics/views.py
+++ multi_stat/statistics_and_analytics/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 
+@login_required()
 def analytics(request):
     return render(request, "statistics_and_analytics/analytics.html")
